a1493867bd54cef6d89e55c74aca683c79b601c4
Joselaine Bechaire <git.bechaire@gmail.com>
2025-12-17 14:22:28 -0300

    fix:correção no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia',
+      entities: [Categoria],
+      synchronize: true, 
+    }),
+   CategoriaModule, 
+  ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+
