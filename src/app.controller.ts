6276886438c6fcd0d4ea27893a5821d9d6f1a870
Joselaine Bechaire <git.bechaire@gmail.com>
2025-12-17 10:37:40 -0300

    feat: add config app.controller/module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
-@Controller()
-export class AppController {
-  constructor() {}
-}
+@Module({
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia',
+      entities: [],
+      synchronize: true, 
+    }),
+    
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
 
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
